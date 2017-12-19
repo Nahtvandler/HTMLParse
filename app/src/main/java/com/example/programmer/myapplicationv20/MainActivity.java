9a02b8bcd77a47ac14d9a07345ae6ac2180c2c17
Misha Larin <hanter715@gmail.com>
2017-12-19 16:29:07 +0300

    dev1

--- app/src/main/java/com/example/programmer/myapplicationv20/MainActivity.java
+++ app/src/main/java/com/example/programmer/myapplicationv20/MainActivity.java
@@ -66,10 +66,10 @@ public class MainActivity extends AppCompatActivity {
                 //Вытаскиваем рассписание
                 Date DNow=new Date();
                 SimpleDateFormat FormatforDateNow = new SimpleDateFormat("yyyy-MM-dd");
-                //System.out.println("текущая дата "+FormatforDateNow.format(DNow));
+                System.out.println("текущая дата "+FormatforDateNow.format(DNow));
 
-                //Document doc = Jsoup.connect("https://home.mephi.ru/study_groups/2888/day?date="+FormatforDateNow.format(DNow)).get();
-                Document doc = Jsoup.connect("https://home.mephi.ru/study_groups/2888/day?date=2017-12-15").get();
+                Document doc = Jsoup.connect("https://home.mephi.ru/study_groups/2888/day?date="+FormatforDateNow.format(DNow)).get();
+                //Document doc = Jsoup.connect("https://home.mephi.ru/study_groups/2888/day?date=2017-12-15").get();
 
                 //list-group-item
                 //Document doc = Jsoup.connect("https://home.mephi.ru/study_groups/2888/day?date=2017-12-13").get();
